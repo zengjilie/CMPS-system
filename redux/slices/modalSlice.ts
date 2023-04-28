e1b6274f3a99c9cbe57d7c7c2a58a6db26a92a15
Jilie(Alex) Zeng <zengjilie@gmail.com>
2023-04-27 23:22:24 -0500

    bug fixed2

---
+++ redux/slices/modalSlice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+export interface ModalState {
+  isFinishTaskModalOpen: boolean;
+  isChangeAnswerModalOpen: boolean;
+}
+
+const initialState: ModalState = {
+  isFinishTaskModalOpen: false,
+  isChangeAnswerModalOpen: false,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    toggleFTModal: (state: ModalState) => {
+      state.isFinishTaskModalOpen = !state.isFinishTaskModalOpen;
+      console.log(state.isFinishTaskModalOpen);
+    },
+    toggleCAModal: (state: ModalState) => {
+      state.isChangeAnswerModalOpen = !state.isChangeAnswerModalOpen;
+    },
+  },
+});
+
+export const { toggleCAModal, toggleFTModal } = modalSlice.actions;
+export default modalSlice.reducer;
