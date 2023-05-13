42d7601b6e6706b3719828ffdb970f3d19588069
Jilie(Alex) Zeng <zengjilie@gmail.com>
2023-05-12 23:03:29 -0500

    update style for warmup

---
+++ lib/api.ts
@@ -0,0 +1,42 @@
+import Axios, { AxiosRequestConfig } from "axios";
+
+const BASE_URL =
+  "https://xqvgyy0ll7.execute-api.ap-northeast-1.amazonaws.com/prod";
+
+interface RequestType {
+  path: string;
+  data: any;
+  params?: any;
+}
+
+export const API = {
+  post: async <T>({ path, data, params }: RequestType) => {
+    const config: AxiosRequestConfig = {
+      method: "post",
+      url: `${BASE_URL}${path}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data,
+      params,
+    };
+
+    const response = await Axios.request<T>(config);
+    return response;
+  },
+
+  patch: async <T>({ path, data, params }: RequestType) => {
+    const config: AxiosRequestConfig = {
+      method: "patch",
+      url: `${BASE_URL}${path}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data,
+      params,
+    };
+
+    const response = await Axios.request<T>(config);
+    return response;
+  },
+};
